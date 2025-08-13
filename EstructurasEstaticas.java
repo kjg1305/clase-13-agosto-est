62e9356a4396602cbe69d183ff7ef558c9656b54
303 - Programacion <303@laboratorios.usbbog.edu.co>
2025-08-13 16:37:52 -0500

    cambio de archvios mas los ejemplos de estructuras estaticas

--- EstructurasEstaticas.java
+++ EstructurasEstaticas.java
@@ -1,5 +1,4 @@
 public class EstructurasEstaticas {
-    @SuppressWarnings("unused")
     public static void main (String[]args){
         // Estruturas estaticas no se cambian  
         int a = 10 ;
@@ -7,5 +6,34 @@ public class EstructurasEstaticas {
         //int a = 10 , b = 11 ;
         int suma = a + b ;
         int resta = a - b ;
+        long c = 300000000000L;
+        float division = a/b ;
+        float multiplicacion = a*b;
+        // Optional divicion = a/b ; operacion para casos especiales , como division por 0 
+        // estrutura char , es para caracteres SOLO un elemento  
+        char K ='A';
+        char j = '4';
+        char P = 'L';
+        char z = 'a';
+        // STRINGS  para palbras 
+        String palabras = "holiiis";
+        //String suma_carac = K+j+P+z ; ERROR 
+        boolean m = true;
+        boolean n = false ;
+
+        //  EJEMPLOS DE CADA ESTRUCTURA 
+        int  numero1 = 10 ;
+        long cifra = 500000000000l;
+        float decimal = numero1/cifra ;
+        char W = '3';
+        String cadena = " adioos";
+        boolean verdad = false ;
+        boolean mentira = verdad ;
+
+
+
+
+
+
     }
 }
--- EstructurasEstaticas.py
+++ EstructurasEstaticas.py
@@ -1,5 +1,28 @@
+# estructuras de datos 
 a = 10 
 b = 11
 a,b = 10,11
 sumas = a+b
 resta = a-b 
+multiplicacion = a*b
+divicion = a/b 
+
+# cararteres  y strings 
+palabra = " holaa"
+for i in palabra : 
+    print(i)
+
+
+h = 'h'
+o = "o"
+l = "l"
+a= "a"
+print (h+o+l+a)
+
+# EJEMPLOS DE ESTRUCTURAS DE DATOS 
+k = 190 
+j = 3.1416
+nombre = " judith"
+verdad = False
+mentira = True 
+m = 'e'
