daf69b0666da2964b5f089d9cbe79eeb9e0a1279
303 - Programacion <303@laboratorios.usbbog.edu.co>
2025-08-13 15:47:57 -0500

    archivos en java y python , sobre las estructuras estaticas de datos

---
+++ EstructurasEstaticas.java
@@ -0,0 +1,11 @@
+public class EstructurasEstaticas {
+    @SuppressWarnings("unused")
+    public static void main (String[]args){
+        // Estruturas estaticas no se cambian  
+        int a = 10 ;
+        int b = 11 ; 
+        //int a = 10 , b = 11 ;
+        int suma = a + b ;
+        int resta = a - b ;
+    }
+}
---
+++ EstructurasEstaticas.py
@@ -0,0 +1,5 @@
+a = 10 
+b = 11
+a,b = 10,11
+sumas = a+b
+resta = a-b 
